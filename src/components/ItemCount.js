d7205b87eea6a89baabe46e3d89a15d3f60dae4f
Juan Gregorio Urrutibeheity <gregogelp10@gmail.com>
2022-02-16 17:53:55 -0300

    desafio5

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,26 +1,26 @@
-import { getListItemIconUtilityClass } from "@mui/material";
-import { maxHeight } from "@mui/system";
 import { useState } from "react";
 
 const  ItemCount = (props) => {
 
     //let valoraciones = 0;
-    const [valoraciones, setValoraciones] = useState(0);
+    const [valoraciones, setValoraciones] = useState(1);
     const increment = () => {
+        if(valoraciones < 5)
         //valoraciones = valoraciones +1;
         setValoraciones(valoraciones+1);
-        console.log(valoraciones)
+        console.log(valoraciones);
     }
-    const desincrement = () => {
+    const decrement = () => {
+        if(valoraciones > 0)
         //valoraciones = valoraciones +1;
         setValoraciones(valoraciones-1);
-        console.log(valoraciones)
+        console.log(valoraciones);
     }
 
     return (
     <><p>{valoraciones} Camisa Tiger</p>
     <button onClick={increment}>Incrementar</button>
-    <button onClick={desincrement}>Desincrementar</button></>
+    <button onClick={decrement}>Desincrementar</button></>
 
     )
 }
